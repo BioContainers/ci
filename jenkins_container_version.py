1319d3d76ce887981e1df2f4c19b00b2a298f954
raynooo <renaud.jullien@gmail.com>
2018-11-07 10:07:32 +0000

    Tests for parsing ARG lines, doesn't do anything special about them

--- jenkins_container_version.py
+++ jenkins_container_version.py
@@ -19,8 +19,38 @@ dfp = DockerfileParser()
 dfp.content = content
 labels = dfp.labels
 
+###DockerfileParser doesn't know how to link ARG declarations to their subsequent use
+###Let's find a way around it
+
+def is_arg_used_here (arg_dict, recipe_line):
+	for crt_arg in arg_dict.keys():
+		if "$"+crt_arg in recipe_line:
+			print (recipe_line)
+
+##Parsing all ARGs
+recipe_args = dict()
+for crtInstr in dfp.structure:
+	if crtInstr['instruction']=="ARG":
+		#print ("Here's an ARG: "+crtInstr['value'])
+		parts = crtInstr['value'].split('=')
+		if len(parts) != 2:
+			print("An ARG instruction is wrongly formatted")
+			sys.exit(1)
+		recipe_args[parts[0]]=parts[1]
+		continue
+	##ARG instructions are at the start of the Dockerfile
+	##Within the same loop we can update the structure using what we've found
+	is_arg_used_here (recipe_args, crtInstr['value'])
+
+#print (dfp.envs)
+#print (dfp.labels)
+#print (recipe_args)
+#print (content)
+#print (dfp.structure)
+
 software_version = None
 if 'TOOL_VERSION' in os.environ:
+    print ("BLORG")
     labels['software.version'] = os.environ['TOOL_VERSION']
 if 'software.version' not in labels or not labels['software.version']:
     print("Failed to found software.version")
