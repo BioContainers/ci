60ea330a7793d3aefca7882aedd135b7da3c41a6
Olivier Sallou <olivier.sallou@irisa.fr>
2021-09-06 14:10:21 +0200

    update env vars and add kubernetes recipes

--- proxy/ci-proxy-service.sh
+++ proxy/ci-proxy-service.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# export GITHUB_STATUS_TOKEN=
+export GITHUB_STATUS_TOKEN=$GITHUB_STATUS_TOKEN
+export JENKINS_URL=$JENKINS_URL
 cd /home/ubuntu
 . ci-proxy/bin/activate
-gunicorn --pid /home/ubuntu/ci-proxy.pid -b 127.0.0.1:8080 --access-logfile /home/ubuntu/ci-proxy.log --error-logfile /home/ubuntu/ci-error.log ci-proxy:app
+gunicorn --pid /home/ubuntu/ci-proxy.pid -b 0.0.0.0:8080 --access-logfile /home/ubuntu/ci-proxy.log --error-logfile /home/ubuntu/ci-error.log ci-proxy:app
 
