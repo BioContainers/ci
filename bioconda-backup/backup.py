63f462d7f10ea1183d1a1248d3fc1b2bdaa5d669
Olivier Sallou <olivier.sallou@irisa.fr>
2021-09-10 08:19:28 +0200

    update backup scripts

--- bioconda-backup/backup.py
+++ bioconda-backup/backup.py
@@ -8,13 +8,11 @@ import docker
 import sys
 import os
 import subprocess
+from email.utils import parsedate_tz
 
-if len(sys.argv) != 3:
-    logging.error('must specify conda package and version')
+if len(sys.argv) != 2:
+    logging.error('must specify yaml file as input')
     sys.exit(1)
-
-CONDA_CONTAINER = sys.argv[1]
-CONDA_TAG = sys.argv[2]
 
 def fake(*args):
     return ''
@@ -23,23 +21,34 @@ docker_client = docker.from_env()
 
 logging.basicConfig(level=logging.INFO)
 
+#Load Jinja2 template
+env = Environment(loader = FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+
+template = env.get_template(sys.argv[1])
+try:
+    res = template.render(compiler=fake, cdt=fake,environ=fake,os=os, pin_compatible=fake, max_pin=fake, exit=fake)
+    data= yaml.load(res, Loader=yaml.FullLoader)
+except Exception as e:
+    logging.error("%s: %s" % (sys.argv[1], str(e)))
+    sys.exit(0)
+
 
 regtags = []
 try:
-    reg = requests.get('http://docker-registry.local:30750/v2/biocontainers/%s/tags/list' % CONDA_CONTAINER)
+    reg = requests.get('http://docker-registry.local:30750/v2/biocontainers/' + data['package']['name'] + '/tags/list')
     if reg.status_code == 200:
       regjson = reg.json()
       regtags = regjson['tags']
 
-    logging.info('%s tags: %s' % (CONDA_CONTAINER, str(regtags)))
+    logging.info(data['package']['name'] + 'tags: ' + str(regtags))
       
 except Exception:
     logging.exception('failed to get tags for container')
     sys.exit(0)
 
-r = requests.get('https://quay.io/api/v1/repository/biocontainers/%s' % CONDA_CONTAINER)
+r = requests.get('https://quay.io/api/v1/repository/biocontainers/' + data['package']['name'])
 if r.status_code != 200:
-    logging.error("[quay.io] Can't access container %s" % (CONDA_CONTAINER))
+    logging.info("Can't access container %s" % (data['package']['name']))
     sys.exit(0)
 
 rdata = r.json()
@@ -48,50 +57,48 @@ tags = rdata['tags']
 cli = docker.APIClient(base_url='unix://var/run/docker.sock')
 last_update = None
 tag = None
-for key, t in tags.items():
-    logging.info("[bioconda][tag=%s]" % t['name'])
-    if t['name'] == CONDA_TAG:
-        tag = CONDA_TAG
-        break
-if tag is None:
-    logging.error('bioconda container tag not found: %s:%s' % (CONDA_CONTAINER, CONDA_TAG))
-    sys.exit(1)
+for key, t in tags.iteritems():
+    last = t['last_modified']
+    last_modified = parsedate_tz(last)
+    if last_update is None or last_modified > last_update:
+        last_update = last_modified
+        tag = t
+if tag:
+    logging.info('bioconda container: ' + data['package']['name'] + ',tag: ' + tag['name'])
 
-logging.info('bioconda container: %s:%s' % (CONDA_CONTAINER, CONDA_TAG))
+    if os.environ.get('FORCETAG', None) is None and tag['name'] in regtags:
+        logging.info('tag already present, skipping')
+        #continue
+        sys.exit(0)
 
-
-pull_ok = False
-try:
-    logging.info('pull quay.io/biocontainers/%s:%s' % (CONDA_CONTAINER, CONDA_TAG))
-    docker_client.images.pull('quay.io/biocontainers/%s:%s' %(CONDA_CONTAINER, CONDA_TAG))
-    logging.info('tag image to docker-registry.local:30750') 
-    cli.tag('quay.io/biocontainers/%s:%s' % (CONDA_CONTAINER, CONDA_TAG), 'docker-registry.local:30750/biocontainers/%s' % CONDA_CONTAINER, tag=CONDA_TAG)
-    pull_ok = True
-except Exception as e:
-    logging.exception('Failed to pull/tag container %s:%s, %s' % (CONDA_CONTAINER, CONDA_TAG, str(e)))
-try:
-    if pull_ok:
-        logging.info('push image docker-registry.local:30750/biocontainers/%s:%s' % (CONDA_CONTAINER, CONDA_TAG))        
-        for line in docker_client.images.push('docker-registry.local:30750/biocontainers/%s' % CONDA_CONTAINER, tag=CONDA_TAG, stream=True):
-            logging.info(str(line))
-        logging.info('push ok')
-except Exception as e:
-    logging.error('Failed to push container %s:%s, %s' % (CONDA_CONTAINER, CONDA_TAG, str(e)))
-
-try:
-    logging.info('add anchore scan')
-    anchore_image = 'quay.io/biocontainers/%s:%s' % (CONDA_CONTAINER, CONDA_TAG)
-    cmd = ['anchore-cli', 'image', 'add', anchore_image]
-    scan = subprocess.check_output(cmd)
-    logging.info('scan output: ' + str(scan))
-except Exception as e:
-    logging.error('Failed to scan container %s:%s, %s' % (CONDA_CONTAINER, CONDA_TAG, str(e)))
-
-try: 
-    logging.info('cleanup of images') 
-    docker_client.images.remove('quay.io/biocontainers/%s:%s' % (CONDA_CONTAINER, CONDA_TAG))
-    docker_client.images.remove('docker-registry.local:30750/biocontainers/%s:%s' % (CONDA_CONTAINER, CONDA_TAG))
-except Exception:
-    logging.warn('failed to delete images for %s:%s' % (CONDA_CONTAINER, CONDA_TAG))
-
+    pull_ok = False
+    try:
+        logging.info('pull quay.io/biocontainers/' + data['package']['name'] + ':' + tag['name'])
+        docker_client.images.pull('quay.io/biocontainers/' + data['package']['name'], tag=tag['name'])
+        logging.info('tag image to docker-registry.local:30750') 
+        cli.tag('quay.io/biocontainers/' + data['package']['name'] + ':' + tag['name'], 'docker-registry.local:30750/biocontainers/' + data['package']['name'], tag=tag['name'])
+        pull_ok = True
+    except Exception as e:
+        logging.error('Failed to pull/tag container %s:%s, %s' % (data['package']['name'], tag['name'], str(e)))
+    try:
+        if pull_ok:
+            logging.info('push image docker-registry.local:30750/biocontainers/' + data['package']['name'] + ':' + tag['name'])        
+            for line in docker_client.images.push('docker-registry.local:30750/biocontainers/' + data['package']['name'], tag=tag['name'], stream=True):
+                logging.debug(str(line))
+            logging.info('push ok')
+            logging.info('add anchore scan')
+            anchore_image = 'quay.io/biocontainers/' + data['package']['name'] + ':' + tag['name']
+            cmd = ['anchore-cli', '--url', os.environ['ANCHORE_CLI_URL'],
+                    '--u', os.environ['ANCHORE_CLI_USER'], '--p',
+                    os.environ['ANCHORE_CLI_PASS'],  'image', 'add', anchore_image]
+            scan = subprocess.check_output(cmd)
+            logging.debug('scan output: ' + str(scan))
+    except Exception as e:
+        logging.error('Failed to push container %s:%s, %s' % (data['package']['name'], tag['name'], str(e)))
+    try: 
+        logging.debug('cleanup of images') 
+        docker_client.images.remove('quay.io/biocontainers/' + data['package']['name'] + ':' + tag['name'])
+        docker_client.images.remove('docker-registry.local:30750/biocontainers/' + data['package']['name'] + ':' + tag['name'])
+    except Exception:
+        logging.warn('failed to delete images for %s:%s' % (data['package']['name'], tag['name']))
 
---
+++ bioconda-backup/backup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+WORKDIR=/home/ubuntu/conda-backup
+
+date
+
+if [ -e $WORKDIR/LOCK ]; then
+    echo "in progress, skipping"
+    exit 1
+fi
+
+touch $WORKDIR/LOCK
+
+cd $WORKDIR
+
+. ./venv/bin/activate
+. ./env.sh
+
+LAST_SHA=
+if [ ! -e bioconda-recipes ]; then
+    echo "new clone"
+    git clone https://github.com/bioconda/bioconda-recipes.git
+    cd bioconda-recipes
+else
+    cd bioconda-recipes
+    if [ -z "$FORCE" ]; then
+        LAST_SHA=`git show HEAD | sed -n 1p | cut -d " " -f 2`
+        echo "Last sha: $LAST_SHA"
+    else
+        echo "Force scan of all repo"
+    fi
+fi
+git pull origin master
+
+if [ "a$LAST_SHA" == "a" ]; then
+    find . -name "meta.yaml" > /tmp/recipes.txt
+else
+    NEW_SHA=`git show HEAD | sed -n 1p | cut -d " " -f 2`
+    echo "New sha: $NEW_SHA"
+    git diff --name-only $LAST_SHA $NEW_SHA | grep "meta.yaml" > /tmp/recipes.txt
+fi
+
+while read p; do
+    echo "backup $p"
+    python $WORKDIR/backup.py $p
+done </tmp/recipes.txt
+
+rm /tmp/recipes.txt
+
+rm $WORKDIR/LOCK
+echo "backup done"
